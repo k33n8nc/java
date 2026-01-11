3e1d58e3fb78e774f489924371e0447410272cfd
k33n8nc <robbert.stapel@live.nl>
2026-01-11 01:26:38 +0100

    Separate responsibility

--- src/main/java/nl/cybernetix/demo/Application.java
+++ src/main/java/nl/cybernetix/demo/Application.java
@@ -1,12 +1,22 @@
 package nl.cybernetix.demo;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+@RequiredArgsConstructor
+public class Application implements CommandLineRunner {
+
+	private final Restaurant restaurant;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		restaurant.open();
 	}
 }
--- src/main/java/nl/cybernetix/demo/Restaurant.java
+++ src/main/java/nl/cybernetix/demo/Restaurant.java
@@ -1,22 +1,17 @@
 package nl.cybernetix.demo;
 
 import nl.cybernetix.demo.actors.Waitress;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 
 @Component
 @AllArgsConstructor
-public class Restaurant implements CommandLineRunner {
+public class Restaurant {
 
     private final Waitress waitress;
 
-    // De run method is verantwoordelijk voor het starten van de applicatie
-    // Het restaurant moet echt enkel restaurant dingen doen.
-    // injecteer runner dus in applicatie en niet hier
-    @Override
-    public void run(String... args) {
+    public void open() {
         System.out.println("Restaurant is open! Starting the process...");
         waitress.setName("Jessica");
         waitress.takeOrder();
