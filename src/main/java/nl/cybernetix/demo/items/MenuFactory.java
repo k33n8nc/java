69e02babdfa66ae6310bf1754edf270dddade705
k33n8nc <robbert.stapel@live.nl>
2026-01-14 00:52:23 +0100

    Let MenuFactory handle creating a new Menu

--- src/main/java/nl/cybernetix/demo/items/MenuFactory.java
+++ src/main/java/nl/cybernetix/demo/items/MenuFactory.java
@@ -1,17 +1,16 @@
 package nl.cybernetix.demo.items;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+import java.util.List;
+
+@Component
 public class MenuFactory {
-    @Bean
-    public Menu createMenu() {
-        Menu menu = new Menu();
-        menu.addMenuItem(new MenuItem("1", "Spaghetti Bolognese", 12.50));
-        menu.addMenuItem(new MenuItem("2", "Margherita Pizza", 10.00));
-        menu.addMenuItem(new MenuItem("3", "Caesar Salad", 8.75));
-        menu.addMenuItem(new MenuItem("4", "Tiramisu", 6.00));
-        return menu;
+    public Menu createMenu(){
+        return new Menu(List.of(
+                new MenuItem("1", "Spaghetti Bolognese", 12.5),
+                new MenuItem("2", "Margherita Pizza", 10.0),
+                new MenuItem("3", "Caesar Salad", 8.75)
+        ));
     }
 }
